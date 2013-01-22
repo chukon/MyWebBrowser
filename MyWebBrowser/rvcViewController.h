ecd68e77065d6956bbaeb9e53621da815ababf79
Charles Konkol <cbkonkol@PC18981.local>
2013-01-21 19:04:16 -0600

    Initial Commit

---
+++ MyWebBrowser/rvcViewController.h
@@ -0,0 +1,13 @@
+//
+//  rvcViewController.h
+//  MyWebBrowser
+//
+//  Created by Charles Konkol on 1/21/13.
+//  Copyright (c) 2013 Charles Konkol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface rvcViewController : UIViewController
+
+@end
---
+++ MyWebBrowser/rvcViewController.m
@@ -0,0 +1,29 @@
+//
+//  rvcViewController.m
+//  MyWebBrowser
+//
+//  Created by Charles Konkol on 1/21/13.
+//  Copyright (c) 2013 Charles Konkol. All rights reserved.
+//
+
+#import "rvcViewController.h"
+
+@interface rvcViewController ()
+
+@end
+
+@implementation rvcViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
